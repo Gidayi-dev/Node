ec7572b4dc537247d06aca2d6578e93a589805c5
Millyannah Gidayi <millyannahi@gmail.com>
2025-03-01 21:16:33 -0800

    http requests

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 import http from 'http';
-const PORT = 4000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
-const server = http.createServer((req, res) =>{
-    res.write('Hello, Gidayi');
-    res.end();
+const PORT = process.env.PORT;
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) =>{
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+    try {
+        if (req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found')
+            }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+                
+        } else {
+            throw new Error('Method not allowed');
+        } 
+    } catch (error) {
+        res.writeHead(404, {'content-type': 'text/plain'})
+    
+            // res.write('<h1>Hello, Gidayi</h1>');
+            res.end('Server Error');
+    }
 });
+
 
 server.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
