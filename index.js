01e2666d4c3c93b0114725930e12ef8c58659cf9
Millyannah Gidayi <millyannahi@gmail.com>
2025-03-01 19:32:05 -0800

    Node

---
+++ index.js
@@ -0,0 +1,10 @@
+import getPosts, {getPostsLength} from "./postControllers.js";
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius To Fahrenheit: ${celciusToFahrenheit(0)}`);
