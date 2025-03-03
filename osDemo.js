d45dbf24754945ca552f5d65239fe2a955f874ad
Millyannah Gidayi <millyannahi@gmail.com>
2025-03-02 23:03:52 -0800

    url & os modules

---
+++ osDemo.js
@@ -0,0 +1,14 @@
+import { log } from 'console';
+import os from 'os';
+
+// userInfo()
+console.log(os.userInfo());
+
+// totalmem()
+console.log(os.totalmem());
+
+// freemem()
+console.log(os.freemem());
+
+//cpus( )
+console.log(os.cpus());
